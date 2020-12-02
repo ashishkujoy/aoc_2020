4376cc6a3aa4e03739e72f3b125686ed55232ce6
Ashish Kumar <ashishku@thoughtworks.com>
2020-12-02 15:12:46 +0530

    Add method to count all invalid passwords

--- src/problems/day_2.rs
+++ src/problems/day_2.rs
@@ -27,8 +27,28 @@ impl<'i> PasswordPolicy<'i> {
             max_occurance: max,
         }
     }
+
+    fn is_adhered_by(&self, password: &str) -> bool {
+        let char_occurance_count = password.split("").filter(|e| e == &self.char).count();
+        (self.min_occurance..self.max_occurance + 1).contains(&(char_occurance_count as u16))
+    }
 }
 
+fn parse_password_and_policy<'i>(input: &'i str) -> (&str, PasswordPolicy<'i>) {
+    let mut inputs = input.split(": ");
+    let password_policy = PasswordPolicy::parse(inputs.next().unwrap());
+    (inputs.next().expect("missing password"), password_policy)
+}
+
+fn get_count_of_invalid_passwords(inputs: &Vec<&str>) -> usize {
+    inputs
+        .into_iter()
+        .filter(|input| {
+            let (password, password_policy) = parse_password_and_policy(input);
+            !password_policy.is_adhered_by(password)
+        })
+        .count()
+}
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -46,5 +66,60 @@ mod tests {
                 max_occurance: 7
             }
         )
+    }
+
+    #[test]
+    fn pass_password_and_password_policy() {
+        let input = "1-7 j: vrfjljjwbsv";
+        let (password, password_policy) = parse_password_and_policy(input);
+
+        assert_eq!(password, "vrfjljjwbsv");
+        assert_eq!(
+            password_policy,
+            PasswordPolicy {
+                char: "j",
+                min_occurance: 1,
+                max_occurance: 7
+            }
+        );
+    }
+
+    #[test]
+    fn check_a_password_having_given_char_between_min_and_max_count_adhere_to_password_policy() {
+        let password_policy = PasswordPolicy {
+            char: "j",
+            min_occurance: 1,
+            max_occurance: 7,
+        };
+
+        assert!(password_policy.is_adhered_by("vrfjljjwbsv"))
+    }
+    #[test]
+    fn check_a_password_having_given_char_between_below_min_count_does_not_adhere_to_password_policy(
+    ) {
+        let password_policy = PasswordPolicy {
+            char: "j",
+            min_occurance: 5,
+            max_occurance: 7,
+        };
+
+        assert!(!password_policy.is_adhered_by("vrfjljjwbsv"))
+    }
+    #[test]
+    fn check_a_password_having_given_char_between_above_max_count_does_not_adhere_to_password_policy(
+    ) {
+        let password_policy = PasswordPolicy {
+            char: "j",
+            min_occurance: 1,
+            max_occurance: 2,
+        };
+
+        assert!(!password_policy.is_adhered_by("vrfjljjwbsv"))
+    }
+
+    #[test]
+    fn give_count_of_password_not_adhering_to_policy() {
+        let inputs = vec!["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"];
+        assert_eq!(get_count_of_invalid_passwords(&inputs), 1);
     }
 }
