b17c1567d69348e6edd0c6f563d4c56aa360ce6f
Ashish Kumar <ashishku@thoughtworks.com>
2020-12-05 13:19:35 +0530

    Remove duplicate logic of getting row or column id

--- src/problems/day_5.rs
+++ src/problems/day_5.rs
@@ -1,23 +1,9 @@
 use std::ops::Range;
 
-fn get_row_number(ticket_number: &str) -> usize {
-    ticket_number[..7]
-        .chars()
-        .fold((0, 127), |range, c| {
-            if c == 'F' {
-                (range.0, mid_of(range))
-            } else {
-                (mid_of(range), range.1)
-            }
-        })
-        .0
-}
-
-fn get_column_number(ticket_number: &str) -> usize {
-    ticket_number[7..10]
-        .chars()
-        .fold((0, 7), |range, c| {
-            if c == 'L' {
+fn get_get_id_for_code(code: &str, lower_bond: usize, upper_bond: usize) -> usize {
+    code.chars()
+        .fold((lower_bond, upper_bond), |range, c| {
+            if c == 'F' || c == 'L' {
                 (range.0, mid_of(range))
             } else {
                 (mid_of(range), range.1)
@@ -27,8 +13,8 @@ fn get_column_number(ticket_number: &str) -> usize {
 }
 
 fn get_seat_id(ticket_number: &str) -> usize {
-    let row = get_row_number(&ticket_number);
-    let column = get_column_number(&ticket_number);
+    let row = get_get_id_for_code(&ticket_number[..7], 0, 127);
+    let column = get_get_id_for_code(&ticket_number[7..10], 0, 7);
     (row * 8) + column
 }
 
@@ -50,12 +36,12 @@ mod test {
 
     #[test]
     fn get_row_number_given_ticket() {
-        assert_eq!(get_row_number("FBFBBFFRLR"), 44);
+        assert_eq!(get_get_id_for_code("FBFBBFF", 0, 127), 44);
     }
 
     #[test]
     fn get_column_number_given_ticket() {
-        assert_eq!(get_column_number("FBFBBFFRLR"), 5);
+        assert_eq!(get_get_id_for_code("RLR", 0, 7), 5);
     }
 
     #[test]
